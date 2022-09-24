90538fbbcebe3fc605b2bfde06f9690336897777
ebazhanov <evg.bazhanov@yahoo.com>
2022-09-24 17:38:12 +0200

    add slider + sass

--- README.md
+++ README.md
@@ -1,8 +1,2 @@
-
-An example how to fetch data from your own [Gist](https://gist.github.com/Ebazhanov/c391d558be432e54dc0f78783879ee84) 
-
-- [DEMO](https://quotes-inspirational.netlify.app/)
-
-- [Codesandbox](https://codesandbox.io/s/inspirational-quotes-from-gist-i6nts)
-
-![screenshot](public/screen.jpg)
+An example how to fetch data from your own Gist
+---
--- src/App.js
+++ src/App.js
@@ -1,44 +1,63 @@
 import React, { useEffect, useState } from "react";
-import Button from "./Button";
-import "./App.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "./App.scss";
+import "./index.css";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { FreeMode, Keyboard, Mousewheel, Navigation, Pagination } from "swiper";
 
 function App() {
-    const [quote, setQuote] = useState("");
-    const [author, setAuthor] = useState("");
-    const [, setIsLoading] = useState(true);
+  const [quotes, setQuotes] = useState([]);
+  const [, setIsLoading] = useState(true);
 
-    const getQuote = () => {
-        setIsLoading(true);
-        fetch(
-            "https://gist.githubusercontent.com/Ebazhanov/c391d558be432e54dc0f78783879ee84/raw/1eeb7469ba08c1708153b9b8ec800b4d4ba7a0bd/quotes.json"
-        )
-            .then((res) => res.json())
-            .then((data) => {
-                const randomQuote = Math.floor(Math.random() * 102);
-                setQuote(data.quotes[randomQuote].quote);
-                setAuthor(data.quotes[randomQuote].author);
-            })
-            .finally(() => {
-                setIsLoading(false);
-            });
-    };
+  const getQuote = () => {
+    setIsLoading(true);
+    fetch(
+      "https://gist.githubusercontent.com/Ebazhanov/b9ca7c520b5fe0cd1284cc457f89b721/raw/786952e5376e5d96b28864554d5e9674f684cc52/quotes-new.json"
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setQuotes(data);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
 
-    useEffect(() => {
-        getQuote();
-    }, []);
+  useEffect(() => {
+    getQuote();
+  }, []);
 
-    return (
-        <div className="boxes">
-            <div className="quote-container">
-                <h1 className="quote-text">{quote}</h1>
-                <p className="quote-author">-{author}</p>
+  return (
+    <div className="s-container">
+      <Swiper
+        cssMode={true}
+        navigation={true}
+        pagination={{ clickable: true }}
+        mousewheel={true}
+        keyboard={true}
+        followFinger={true}
+        loop={true}
+        freeMode={true}
+        modules={[Navigation, Pagination, Mousewheel, Keyboard, FreeMode]}
+        className="swiper-slide"
+      >
+        {quotes.map((quote, i) => (
+          <SwiperSlide>
+            <div className="s-right">
+              <p className="s-desc">{quote.author}</p>
+              <h1 className="s-title">{quote.quote}</h1>
             </div>
-            <Button changeOnClick={getQuote}>next quote</Button>
-            <footer>
-                <div className="footer-text">© 2021 Ebazhanov</div>
-            </footer>
-        </div>
-    );
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <footer>
+        <div className="footer-text">© 2021 Ebazhanov</div>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
